f5cc0f1745498f4b0cdf54d6b01c11ff00431cef
Hiten Garg <hitengarg45@gmail.com>
2020-08-23 14:36:31 +0530

    Nav Changes

--- src/utils/navbar.jsx
+++ src/utils/navbar.jsx
@@ -3,19 +3,13 @@ import React from "react";
 const NavBar = () => (
     <div className="bg-dark py-2 px-2">
         <span>
-            <a
-                className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
-                href="https://github.com/dipeshpatil/algorithms-visualiser"
-            >
+            <a className="btn-sm text-decoration-none bg-dark rounded-0 text-light" href="https://github.com/hitengarg45">
                 <i className="fab text-light fa-github"></i>
                 &nbsp; Repo URL
             </a>
-            <a
-                className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
-                href="https://linkedin.com/in/dipesh-patil"
-            >
+            <a className="btn-sm text-decoration-none bg-dark rounded-0 text-light" href="https://www.linkedin.com/in/hiten-garg-4a9385169/">
                 <i className="fab text-light fa-linkedin"></i>
-                &nbsp; Dipesh Patil
+                &nbsp; Hiten Garg
             </a>
         </span>
     </div>
